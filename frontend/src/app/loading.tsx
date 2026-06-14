b7d0a652d766d615f628b3ae2ca80203d500e923
Dhayananthan D <dhayananthan0812@gmail.com>
2026-06-14 19:19:54 +0530

    Add Loading component for loading state

---
+++ frontend/src/app/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="animate-pulse text-3xl font-serif">
+        Murgdur
+      </div>
+    </div>
+  );
+}
